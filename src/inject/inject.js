e6b92bd9d158f5ff5a71eee2a626bbe554ffdff1
Gean <gean.marroquin@gmail.com>
2019-09-08 09:40:55 -0500

    feat: add toggle shortcut

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -19,17 +19,27 @@ chrome.extension.sendMessage({}, function (response) {
           console.error(error)
         },
         success: function (data) {
-          window.$("form .card-box").each(function (index, questionBox) {
-            var answers = data.data;
-            if (console.debug) {
-              console.debug(answers)
+          var answers = data.data;
+          var flag = false;
+
+          // alt + g
+          $(document).on("keyup", function (e) {
+            if (e.altKey && e.which == 71) {
+              flag = !flag
             }
+          });
+          window.$("form .card-box").each(function (index, questionBox) {
             var $correctAnswer = window.$(questionBox).find("input[value='" + answers[index] + "']");
             var $label = $correctAnswer.parent().find("label")
             var labelText = $label.html().trim();
             var customLabel = labelText.slice(0, labelText.length - 1);
+
             $label.on("mouseenter", function () {
-              $label.html(customLabel);
+              if (flag) {
+                $label.html(customLabel);
+              } else {
+                $label.html(labelText);
+              }
             })
             $label.on("mouseleave", function () {
               $label.html(labelText);
